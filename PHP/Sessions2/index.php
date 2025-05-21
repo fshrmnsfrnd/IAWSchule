6ac32dac0805f662715dc5702dee6a2c0d72dfd3
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-05-21 09:37:13 +0200

    added user at comment

--- PHP/Sessions2/index.php
+++ PHP/Sessions2/index.php
@@ -24,6 +24,7 @@
             if($user == $_GET['userName'] && $pwd == $_GET['pwd']){
                 $html = str_replace("{site}", $commentsHTML, $page);
                 $_SESSION['loggedIn'] = true;
+                $_SESSION['userName'] = $_GET['userName'];
                 break;
             }else{
                 $html = str_replace("{site}", $login, $page);
@@ -38,7 +39,7 @@
         $html = str_replace("{site}", $commentsHTML, $page);
 
         if(isset($_GET['commentHeader']) && isset($_GET['commentText'])){
-            $commenthtml = "<div><h2>" . $_GET['commentHeader'] . "</h2><p>" . $_GET['commentText'] . "</p></div>";
+            $commenthtml = "<div><i>" . $_SESSION['userName'] . "</i><h2>" . $_GET['commentHeader'] . "</h2><p>" . $_GET['commentText'] . "</p></div>";
             $commentsFile = fopen("./comments.html", "a");
             fputs($commentsFile, $commenthtml);
             fclose($commentsFile);
